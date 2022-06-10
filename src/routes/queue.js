675900c2e72765dac4d6c4559d04894bddce7ec8
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-10 18:22:41 -0400

    Added self join on users table to get mentor AND student names

--- src/routes/queue.js
+++ src/routes/queue.js
@@ -5,9 +5,10 @@ module.exports = db => {
   router.get("/:status", (request, response) => {
     db.query(
       `
-      SELECT workorders.*, users.first_name as student_first_name, users.last_name as student_last_name, categories.description, modules.* 
+      SELECT workorders.*, user_info.first_name as student_first_name, user_info.last_name as student_last_name, mentor_info.first_name as mentor_first_name, mentor_info.last_name as mentor_last_name, categories.description, modules.* 
       FROM workorders 
-      JOIN users ON (users.id = user_student_id)
+      JOIN users user_info ON (user_info.id = user_student_id)     
+      JOIN users mentor_info ON (mentor_info.id = user_mentor_id)   
       JOIN categories ON (categories.id = category_id)
       JOIN modules ON (modules.id = module_id)
       WHERE status_id = $1
