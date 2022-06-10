03f6692c864b4ca4531b3165a4ca28ade89ca451
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-10 18:09:18 -0400

    Update queue route to include student name

--- src/routes/queue.js
+++ src/routes/queue.js
@@ -4,7 +4,15 @@ const router = express.Router();
 module.exports = db => {
   router.get("/:status", (request, response) => {
     db.query(
-      `SELECT * FROM workorders WHERE status_id = $1`, [request.params.status]
+      `
+      SELECT workorders.*, users.first_name as student_first_name, users.last_name as student_last_name, categories.description, modules.* 
+      FROM workorders 
+      JOIN users ON (users.id = user_student_id)
+      JOIN categories ON (categories.id = category_id)
+      JOIN modules ON (modules.id = module_id)
+      WHERE status_id = $1
+      
+      `, [request.params.status]
     ).then(({ rows: res }) => {
       response.json(res);
     }).catch((err) => {
@@ -15,3 +23,5 @@ module.exports = db => {
 
   return router;
 };
+
+//WHERE status_id = $1
