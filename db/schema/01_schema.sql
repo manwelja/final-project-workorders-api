ab657baf826bcd9b53092c619cb46cf26ef62b31
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-13 14:04:11 -0400

    Update workorder table schema so status defaults to 1

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -48,7 +48,7 @@ CREATE TABLE workorders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_student_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   user_mentor_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  status_id INTEGER REFERENCES statuses(id) ON DELETE CASCADE,
+  status_id INTEGER DEFAULT 1 REFERENCES statuses(id) ON DELETE CASCADE,
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   module_id INTEGER REFERENCES modules(id) ON DELETE CASCADE,
   environment VARCHAR(255),
