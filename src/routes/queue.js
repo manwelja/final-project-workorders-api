792c401fd85bd1fb3d1c0e1b9dbbff261b4ae88e
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-17 18:16:58 -0400

    Update SQL selects to retrieve status text

--- src/routes/queue.js
+++ src/routes/queue.js
@@ -5,13 +5,15 @@ module.exports = db => {
   router.get("/:status", (request, response) => {
     db.query(
       `
-      SELECT workorders.*, user_info.first_name as student_first_name, user_info.last_name as student_last_name, mentor_info.first_name as mentor_first_name, mentor_info.last_name as mentor_last_name, categories.description as category, modules.week, modules.day, modules.topic
+      SELECT workorders.*, user_info.first_name as student_first_name, user_info.last_name as student_last_name, mentor_info.first_name as mentor_first_name, mentor_info.last_name as mentor_last_name, categories.description as category, modules.week, modules.day, modules.topic, statuses.description as status_description 
       FROM workorders 
       LEFT JOIN users user_info ON (user_info.id = user_student_id)     
       LEFT JOIN users mentor_info ON (mentor_info.id = user_mentor_id)   
       LEFT JOIN categories ON (categories.id = category_id)
       LEFT JOIN modules ON (modules.id = module_id)
+      LEFT JOIN statuses on (statuses.id = status_id)
       WHERE status_id = $1
+      ORDER BY workorders.date_created
       
       `, [request.params.status]
     ).then(({ rows: res }) => {
--- src/routes/workorders.js
+++ src/routes/workorders.js
@@ -5,12 +5,13 @@ module.exports = (db, updateWorkorder) => {
   router.get("/", (request, response) => {
     db.query(
       `     
-        SELECT workorders.*, user_info.first_name as student_first_name, user_info.last_name as student_last_name, mentor_info.first_name as mentor_first_name, mentor_info.last_name as mentor_last_name, categories.description as category, modules.week, modules.day, modules.topic 
+        SELECT workorders.*, user_info.first_name as student_first_name, user_info.last_name as student_last_name, mentor_info.first_name as mentor_first_name, mentor_info.last_name as mentor_last_name, categories.description as category, modules.week, modules.day, modules.topic, statuses.description as status_description  
         FROM workorders
         LEFT JOIN users user_info ON (user_info.id = user_student_id)
         LEFT JOIN users mentor_info ON (mentor_info.id = user_mentor_id)
         LEFT JOIN categories ON (categories.id = category_id)
         LEFT JOIN modules ON (modules.id = module_id)
+        LEFT JOIN statuses on (statuses.id = status_id)
         ORDER BY workorders.id
       `
     ).then(({ rows: res }) => {
@@ -21,16 +22,17 @@ module.exports = (db, updateWorkorder) => {
     });
   });
 
-  //Get a workorders by its unique ID
+  //Get a workorder by its unique ID
   router.get("/:id", (request, response) => {
     db.query(
       `      
-        SELECT workorders.*, user_info.first_name as student_first_name, user_info.last_name as student_last_name, mentor_info.first_name as mentor_first_name, mentor_info.last_name as mentor_last_name, categories.description as category, modules.week, modules.day, modules.topic 
+        SELECT workorders.*, user_info.first_name as student_first_name, user_info.last_name as student_last_name, mentor_info.first_name as mentor_first_name, mentor_info.last_name as mentor_last_name, categories.description as category, modules.week, modules.day, modules.topic, statuses.description as status_description 
         FROM workorders
         LEFT JOIN users user_info ON (user_info.id = user_student_id)
         LEFT JOIN users mentor_info ON (mentor_info.id = user_mentor_id)
         LEFT JOIN categories ON (categories.id = category_id)
         LEFT JOIN modules ON (modules.id = module_id)
+        LEFT JOIN statuses on (statuses.id = status_id)
         WHERE workorders.id = $1
       `, [request.params.id]
     ).then(({ rows: res }) => {
@@ -46,22 +48,26 @@ module.exports = (db, updateWorkorder) => {
     let dbQuery = "";
     if (request.params.role === "mentor") {
       dbQuery = `     
-        SELECT workorders.*, user_info.first_name as student_first_name, user_info.last_name as student_last_name, mentor_info.first_name as mentor_first_name, mentor_info.last_name as mentor_last_name, categories.description as category, modules.week, modules.day, modules.topic 
+        SELECT workorders.*, user_info.first_name as student_first_name, user_info.last_name as student_last_name, mentor_info.first_name as mentor_first_name, mentor_info.last_name as mentor_last_name, categories.description as category, modules.week, modules.day, modules.topic, statuses.description as status_description  
         FROM workorders
         LEFT JOIN users user_info ON (user_info.id = user_student_id)
         LEFT JOIN users mentor_info ON (mentor_info.id = user_mentor_id)
         LEFT JOIN categories ON (categories.id = category_id)
         LEFT JOIN modules ON (modules.id = module_id)
-        WHERE user_mentor_id = $1`;
+        LEFT JOIN statuses on (statuses.id = status_id)
+        WHERE user_mentor_id = $1
+        ORDER BY workorders.date_created DESC`;
     } else if (request.params.role === "student") {
       dbQuery = ` 
-        SELECT workorders.*, user_info.first_name as student_first_name, user_info.last_name as student_last_name, mentor_info.first_name as mentor_first_name, mentor_info.last_name as mentor_last_name, categories.description as category, modules.week, modules.day, modules.topic 
+        SELECT workorders.*, user_info.first_name as student_first_name, user_info.last_name as student_last_name, mentor_info.first_name as mentor_first_name, mentor_info.last_name as mentor_last_name, categories.description as category, modules.week, modules.day, modules.topic, statuses.description as status_description  
         FROM workorders
         LEFT JOIN users user_info ON (user_info.id = user_student_id)
         LEFT JOIN users mentor_info ON (mentor_info.id = user_mentor_id)
         LEFT JOIN categories ON (categories.id = category_id)
         LEFT JOIN modules ON (modules.id = module_id)
-        WHERE user_student_id = $1`;
+        LEFT JOIN statuses on (statuses.id = status_id)
+        WHERE user_student_id = $1
+        ORDER BY workorders.date_created DESC`;
     }
     db.query(
       dbQuery, [request.params.id]
@@ -81,11 +87,11 @@ module.exports = (db, updateWorkorder) => {
       `INSERT INTO workorders (${fields}) VALUES (${ref})`, values
     ).then(({ rows: res }) => {
       setTimeout(() => {
-        //call function to send updated workorder data to all connected clients, via websockets        
+        //call function to send updated workorder data to all connected clients, via websockets
         response.status(204).json({});
         updateWorkorder(request.body);
       }, 1000);
-     // response.json(res);
+    // response.json(res);
     }).catch((err) => {
       console.log(err);
       response.json([]);
