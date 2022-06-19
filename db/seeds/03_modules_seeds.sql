4df7123a5edc4a3c1d36353bd0f3b38d68229125
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-19 18:39:00 -0400

    Update schema and seed data

---
+++ db/seeds/03_modules_seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO modules (week, day, topic, archive)
+VALUES
+(0, 0, 'Other...', false),
+(0, 0, 'Ajax', false),
+(0, 0, 'CSS', false),
+(0, 0, 'Cypress', false),
+(0, 0, 'Express', false),
+(0, 0, 'GIT', false),
+(0, 0, 'HTML', false),
+(0, 0, 'JavaScript', false),
+(0, 0, 'Jest', false),
+(0, 0, 'jQuery', false),
+(0, 0, 'Node', false),
+(0, 0, 'PostgreSQL', false),
+(0, 0, 'Rails', false),
+(0, 0, 'React', false),
+(0, 0, 'Ruby', false),
+(0, 0, 'SQL', false),
+(0, 0, 'StoryBook', false);
