ffcffad87f85a57ad450c8c823b7f90e226e089a
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-21 15:49:29 -0400

    Remove cohort reference from workorders table

--- db/seeds/04_users_seeds.sql
+++ db/seeds/04_users_seeds.sql
@@ -1,9 +1,9 @@
-INSERT INTO users (first_name, last_name, email, cohort_id, handle, avatar, role, active, password, join_date)
+INSERT INTO users (first_name, last_name, email, handle, avatar, role, active, password, join_date)
 VALUES
-('Aaron', 'Au', 'aaron@gmail.com', 1, 'aau', '', 'student', true, 'password', '2018-02-12'),
-('Jane', 'Doe', 'janedoe@gmail.com', 1, 'jndoe', '', 'student', true, 'password', '2021-04-06'),
-('Anne', 'Smith', 'smitty@yahoo.com', 1, 'asmith', '', 'mentor', true, 'password', '2021-06-25'),
-('Patricia', 'Anderson', 'pattya@hotmail.com', 2, 'panderson', '', 'mentor', true, 'password', '2021-10-28'),
-('Emma', 'Grannnis', 'emma@gmail.com', 2, 'egrannis', '', 'mentor', true, 'password', '2022-01-01');
+('Aaron', 'Au', 'aaron@gmail.com', 'aau', '', 'student', true, 'password', '2018-02-12'),
+('Jane', 'Doe', 'janedoe@gmail.com', 'jndoe', '', 'student', true, 'password', '2021-04-06'),
+('Anne', 'Smith', 'smitty@yahoo.com', 'asmith', '', 'mentor', true, 'password', '2021-06-25'),
+('Patricia', 'Anderson', 'pattya@hotmail.com', 'panderson', '', 'mentor', true, 'password', '2021-10-28'),
+('Emma', 'Grannnis', 'emma@gmail.com', 'egrannis', '', 'mentor', true, 'password', '2022-01-01');
 
   
