a02f1749a92e7ad1622a01415b0ef7e6c9ee6325
Jennifer Manwell <manwell.fortier@gmail.com>
2022-06-22 11:17:15 -0400

    Update seed data

--- db/seeds/04_users_seeds.sql
+++ db/seeds/04_users_seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (first_name, last_name, email, handle, avatar, role, active, password, join_date)
 VALUES
 ('Aaron', 'Au', 'aaron@gmail.com', 'aau', '', 'student', true, 'password', '2018-02-12'),
-('Jane', 'Doe', 'janedoe@gmail.com', 'jndoe', '', 'student', true, 'password', '2021-04-06'),
+('Jen', 'Manwell', 'jen@gmail.com', 'jmanwell', '', 'student', true, 'password', '2021-04-06'),
 ('Anne', 'Smith', 'smitty@yahoo.com', 'asmith', '', 'mentor', true, 'password', '2021-06-25'),
 ('Patricia', 'Anderson', 'pattya@hotmail.com', 'panderson', '', 'mentor', true, 'password', '2021-10-28'),
 ('Emma', 'Grannis', 'emma@gmail.com', 'egrannis', '', 'mentor', true, 'password', '2022-01-01');
